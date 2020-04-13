5e4c2ee77903a6c6f979607a0376e54a32eb30bf
Chris O'Brien <chrisobrien2000@outlook.com>
2020-04-13 10:11:36 -0500

    Added Method That Returns New List using LINQ

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WordWithTH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in substring to search for");
+            var substringToFind = Console.ReadLine();
+            SubstringIsolater find = new SubstringIsolater(substringToFind);
+
+            var wordsContainingSubstring = find.ReturnStringsContainingSubstring();
+
+            foreach(string word in wordsContainingSubstring)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ SubstringIsolater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace WordWithTH
+{
+    class SubstringIsolater
+    {
+        public string SubstringToFind { get; set; }
+        List<string> words;
+        public SubstringIsolater(string substringToFind)
+        {
+            SubstringToFind = substringToFind.ToLower();
+            words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+        }
+
+        public List<string> ReturnStringsContainingSubstring()
+        {
+            var wordsContainingSubstring = words.Where(s => s.Contains(SubstringToFind)).ToList();
+            return wordsContainingSubstring;
+        }
+    }
+}
